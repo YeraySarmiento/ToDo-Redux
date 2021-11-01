111c64432c6d5e486e00bf4030f0939c7a0961d9
ysarmiento <yeray.sarmiento@hotmail.com>
2021-11-01 17:39:04 +0100

    Add thunk

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,11 +1,23 @@
 import Form from "../../components/Form/Form";
+import List from "../../components/List/List";
 
 const HomePage = () => {
+  const list = [
+    {
+      text: "hola",
+      done: false,
+    },
+    {
+      text: "hello",
+      done: false,
+    },
+  ];
+
   return (
     <>
       <h2>TO DO LIST</h2>
       <Form />
-      
+      <List tasksList={list} />
     </>
   );
 };
