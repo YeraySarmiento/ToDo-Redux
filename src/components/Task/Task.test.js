00bd67396b74aa3d07c7645172f23bd589227034
ysarmiento <yeray.sarmiento@hotmail.com>
2021-11-01 16:55:10 +0100

    Task_Add test

---
+++ src/components/Task/Task.test.js
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import Task from "./Task";
+
+describe("Given a Task Component", () => {
+  describe("When it receives an object with a text: 'Do shopping'", () => {
+    test("Then it should render a <li> tag with the text 'Do shopping' on it", () => {
+      const task = {
+        text: "Do shopping",
+        done: false,
+      };
+
+      render(<Task task={task} />);
+      const taskItem = screen.getByRole("listitem", { name: "list-item" });
+      // const taskText = screen.getByText(task.text);
+
+      expect(taskItem).toHaveTextContent(task.text);
+    });
+  });
+});
