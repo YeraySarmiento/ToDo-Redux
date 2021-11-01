0abb397f2c223707c893a6841e2915bace50bf3a
ysarmiento <yeray.sarmiento@hotmail.com>
2021-11-01 16:14:51 +0100

    Add Form Component

---
+++ src/components/Form/Form.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const Form = () => {
+  const initialData = {
+    text: "",
+    done: false,
+  };
+
+  const [doTask, setDoTask] = useState(initialData);
+
+  const onChange = (event) => {
+    setDoTask({ ...doTask, [event.target.id]: event.target.value });
+  };
+
+  const onCreate = (event) => {
+    event.preventDefault();
+    console.log(doTask);
+  };
+
+  return (
+    <form
+      className="form-create"
+      autoComplete="off"
+      onSubmit={onCreate}
+      noValidate
+    >
+      <div className="form-group">
+        <label htmlFor="name">Task:</label>
+        <input type="text" id="text" value={doTask.text} onChange={onChange} />
+      </div>
+      <div className="button-container">
+        <button type="submit">Create</button>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
