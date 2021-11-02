77125287be49b75d75eb58db4d9829e942557eaa
ysarmiento <yeray.sarmiento@hotmail.com>
2021-11-02 21:38:38 +0100

    Form click Submit test

--- src/components/Form/Form.test.js
+++ src/components/Form/Form.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Provider } from "react-redux";
 import configureStore from "../../redux/store/configureStore";
 import Form from "./Form";
@@ -19,6 +20,24 @@ describe("Given a Form component", () => {
 
       expect(formInput).toBeInTheDocument();
       expect(formButton).toBeInTheDocument();
+    });
+  });
+
+  describe("When the user types in the input", () => {
+    test("Then it should render a non disabled submit button", () => {
+      const tasksStore = configureStore();
+
+      render(
+        <Provider store={tasksStore}>
+          <Form />
+        </Provider>
+      );
+
+      const textInput = screen.getByRole("textbox", { name: /input/i });
+      const button = screen.getByRole("button", { name: /create/i });
+
+      userEvent.type(textInput, "Take the dog to vet");
+      expect(button).not.toBeDisabled();
     });
   });
 });
